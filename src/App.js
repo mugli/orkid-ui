69a5b9a92f4f52de6775cff5eaca507b3b3f7650
Mehdi Hasan <mhasan@omicronlab.com>
2019-03-19 00:19:26 +0600

    Add dummy task table

--- src/App.js
+++ src/App.js
@@ -18,9 +18,12 @@ import {
   Intent,
   Switch,
   Divider,
+  Breadcrumbs,
   ButtonGroup,
   Elevation
 } from '@blueprintjs/core';
+
+const ITEMS = [{ href: '#', text: 'Queues' }, { href: '#', text: 'insert-to-elasticsearch' }];
 
 class App extends Component {
   render() {
@@ -28,7 +31,9 @@ class App extends Component {
       <div>
         <Navbar>
           <NavbarGroup align={Alignment.LEFT}>
-            <NavbarHeading className={Classes.LARGE}>Orkid Dashboard</NavbarHeading>
+            <NavbarHeading style={{ color: Colors.COBALT5 }} className={Classes.LARGE}>
+              <h4>Orkid Dashboard</h4>
+            </NavbarHeading>
             <NavbarDivider />
             <Button className={Classes.MINIMAL} icon="home" text="Queues" />
             <Button className={Classes.MINIMAL} icon="updated" text="Result List" />
@@ -99,6 +104,76 @@ class App extends Component {
               </tr>
             </tbody>
           </table>
+
+          <Breadcrumbs items={ITEMS} />
+
+          <table
+            className="bp3-html-table bp3-html-table-bordered bp3-html-table-striped"
+            style={{ marginTop: '50px', width: '100%', tableLayout: 'fixed' }}>
+            <thead>
+              <tr>
+                <th>id</th>
+                <th>dedup key</th>
+                <th>retry count</th>
+                <th style={{ width: '25%' }}>data</th>
+                <th style={{ width: '25%' }}>result</th>
+                <th style={{ width: '25%' }}>error</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td style={{ wordBreak: 'break-all' }} className={Classes.MONOSPACE_TEXT}>
+                  1552925211012-0
+                </td>
+                <td style={{ wordBreak: 'break-all' }}>null</td>
+                <td>5</td>
+                <td>
+                  <pre style={{ whiteSpace: 'pre-wrap', wordWrap: 'break-word' }}>
+                    {JSON.stringify(
+                      {
+                        firstName: 'Bud',
+                        lastName: 'Hayes',
+                        email: 'Petra84@yahoo.com',
+                        body:
+                          'Consectetur qui qui at. Iste quibusdam voluptas voluptatem eum quasi. Debitis ullam doloremque aspernatur similique.'
+                      },
+                      null,
+                      2
+                    )}
+                  </pre>
+                </td>
+                <td>
+                  <pre style={{ whiteSpace: 'pre-wrap', wordWrap: 'break-word' }}>
+                    {JSON.stringify(
+                      {
+                        done: true
+                      },
+                      null,
+                      2
+                    )}
+                  </pre>
+                </td>
+                <td>
+                  <pre style={{ whiteSpace: 'pre-wrap', wordWrap: 'break-word' }}>
+                    {JSON.stringify(
+                      {
+                        name: 'Error',
+                        message: 'violet',
+                        stack:
+                          'Error: violet\n    at getErrorDetails (/Users/mehdi/work/Projects/orkid-api/utils/seed.js:141:13)\n    at seedFailed (/Users/mehdi/work/Projects/orkid-api/utils/seed.js:78:18)\n    at seed (/Users/mehdi/work/Projects/orkid-api/utils/seed.js:23:41)\n    at process._tickCallback (internal/process/next_tick.js:68:7)'
+                      },
+                      null,
+                      2
+                    )}
+                  </pre>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+
+          <div style={{ textAlign: 'center', marginBottom: '30px', marginTop: '30px' }}>
+            <Button icon="refresh">Load More</Button>
+          </div>
         </div>
       </div>
     );
