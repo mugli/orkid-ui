1f5539bb173a696839da71ae989cb85192989ef5
Mehdi Hasan <mhasan@omicronlab.com>
2019-03-21 02:00:28 +0600

    Handle pagination in taskfeed

--- src/Queues/DataContainer.js
+++ src/Queues/DataContainer.js
@@ -7,9 +7,9 @@ import gql from 'graphql-tag';
 
 class QueueDataContainer extends Component {
   GET_QUEUE_DATA = gql`
-    query Queue($queueName: String!) {
+    query Queue($queueName: String!, $nextCursor: String) {
       queue(name: $queueName) {
-        taskFeed(limit: 3) {
+        taskFeed(limit: 3, nextCursor: $nextCursor) {
           hasNextPage
           nextCursor
           tasks {
@@ -36,11 +36,46 @@ class QueueDataContainer extends Component {
     this.state = { hasNextPage: false, nextCursor: undefined };
   }
 
+  availableNextCursor = undefined;
+  cursorStack = [];
+
+  nextHandler = () => {
+    const { hasNextPage } = this.state;
+
+    if (hasNextPage) {
+      this.cursorStack.push(this.state.nextCursor);
+      this.setState({ nextCursor: this.availableNextCursor });
+    }
+  };
+
+  previousHandler = () => {
+    if (this.cursorStack.length) {
+      const lastCursor = this.cursorStack.pop();
+      this.setState({ nextCursor: lastCursor });
+    }
+  };
+
+  handleQueryComplete = ({
+    queue: {
+      taskFeed: { hasNextPage, nextCursor }
+    }
+  }) => {
+    this.availableNextCursor = nextCursor;
+
+    if (this.state.hasNextPage !== hasNextPage) {
+      this.setState({ hasNextPage });
+    }
+  };
+
   render() {
     const { queueName } = this.props;
+    const { nextCursor } = this.state;
 
     return (
-      <Query query={this.GET_QUEUE_DATA} variables={{ queueName }}>
+      <Query
+        query={this.GET_QUEUE_DATA}
+        variables={{ queueName, nextCursor }}
+        onCompleted={data => this.handleQueryComplete(data)}>
         {({ data, loading, error }) => {
           if (loading) {
             return null;
@@ -51,7 +86,16 @@ class QueueDataContainer extends Component {
           }
 
           const { queue } = data;
-          return <TaskFeed tasks={queue.taskFeed.tasks} />;
+
+          return (
+            <TaskFeed
+              tasks={queue.taskFeed.tasks}
+              hasNextPage={queue.taskFeed.hasNextPage}
+              hasPrevPage={!!this.cursorStack.length}
+              nextHandler={this.nextHandler}
+              previousHandler={this.previousHandler}
+            />
+          );
         }}
       </Query>
     );
--- src/TaskFeed/index.js
+++ src/TaskFeed/index.js
@@ -6,37 +6,49 @@ import Task from './Task';
 
 class TaskFeed extends Component {
   render() {
-    const { tasks } = this.props;
+    const { tasks, hasNextPage, hasPrevPage, nextHandler, previousHandler } = this.props;
 
-    return [
-      <div style={{ textAlign: 'right' }}>
-        <Button icon="arrow-left">Previous</Button>
-        <Button icon="arrow-right">Next</Button>
-      </div>,
-      <table
-        className="bp3-html-table bp3-html-table-bordered bp3-html-table-striped"
-        style={{ width: '100%', tableLayout: 'fixed' }}>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Dedup Key</th>
-            <th>Retry Count</th>
-            <th style={{ width: '25%' }}>Data</th>
-            <th style={{ width: '25%' }}>Result</th>
-            <th style={{ width: '25%' }}>Error</th>
-          </tr>
-        </thead>
-        <tbody>
-          {tasks.map(task => (
-            <Task task={task} key={task.id} />
-          ))}
-        </tbody>
-      </table>,
-      <div style={{ textAlign: 'right', marginBottom: '30px', marginTop: '30px' }}>
-        <Button icon="arrow-left">Previous</Button>
-        <Button icon="arrow-right">Next</Button>
+    return (
+      <div>
+        <div style={{ textAlign: 'right' }}>
+          <Button icon="arrow-left" disabled={!hasPrevPage} onClick={previousHandler}>
+            Previous
+          </Button>
+          <Button icon="arrow-right" disabled={!hasNextPage} onClick={nextHandler}>
+            Next
+          </Button>
+        </div>
+
+        <table
+          className="bp3-html-table bp3-html-table-bordered bp3-html-table-striped"
+          style={{ width: '100%', tableLayout: 'fixed' }}>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Dedup Key</th>
+              <th>Retry Count</th>
+              <th style={{ width: '25%' }}>Data</th>
+              <th style={{ width: '25%' }}>Result</th>
+              <th style={{ width: '25%' }}>Error</th>
+            </tr>
+          </thead>
+          <tbody>
+            {tasks.map(task => (
+              <Task task={task} key={task.id} />
+            ))}
+          </tbody>
+        </table>
+
+        <div style={{ textAlign: 'right', marginBottom: '30px', marginTop: '30px' }}>
+          <Button icon="arrow-left" disabled={!hasPrevPage} onClick={previousHandler}>
+            Previous
+          </Button>
+          <Button icon="arrow-right" disabled={!hasNextPage} onClick={nextHandler}>
+            Next
+          </Button>
+        </div>
       </div>
-    ];
+    );
   }
 }
 
