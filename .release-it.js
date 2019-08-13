527a8501311d563c02fa80fcb564928ef99aa5da
Mehdi Hasan <mhasan@omicronlab.com>
2019-08-14 01:28:06 +0600

    Update circle config

--- .release-it.js
+++ .release-it.js
@@ -16,7 +16,7 @@ module.exports = {
     tagAnnotation: 'Release ${version}',
     tagArgs: '',
     push: true,
-    pushArgs: 'v${version}'
+    pushArgs: '--follow-tags'
   },
   npm: {
     publish: true
